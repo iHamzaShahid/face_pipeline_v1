13b3f5c1fb63533c0aa623e217d81a1eb9ac828f
Danyal Saqib <dsaqib.bee17seecs@seecs.edu.pk>
2021-12-23 15:40:36 +0500

    Create get_model.py

---
+++ models/get_model.py
@@ -0,0 +1,26 @@
+import subprocess
+import os
+
+os.chdir("models")
+# Downloading pretrained models
+if os.path.isfile('arcface.onnx'):
+  print("File 'arcface.onnx' already exists")
+else:
+  subprocess.run(['gdown', '--id', '16aJ_uiDWeggv0V7i9VBSzrdIOUhK84Qr'])
+
+if os.path.isfile('onet.onnx'):
+  print("File 'onet.onnx' already exists")
+else:
+  subprocess.run(['gdown', '--id', '1ESxGRs96gxwp0LT9K1lpU9BzLATidmvS'])
+
+if os.path.isfile('rnet.onnx'):
+  print("File 'rnet.onnx' already exists")
+else:
+  subprocess.run(['gdown', '--id', '1JvcVrFETQ9YOaMJ_ce5H4vtkBeo_orZs'])
+
+if os.path.isfile('pnet.onnx'):
+  print("File 'pnet.onnx' already exists")
+else:
+  subprocess.run(['gdown', '--id', '1ZOJaIQGHxDIxErYiReB09tSYRF793POz'])
+
+os.chdir("..")
